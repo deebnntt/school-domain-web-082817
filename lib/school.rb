58eab408fceafbb8d95a92fe5611c2c2520be48c
deebnntt <daniellebennettchan@gmail.com>
2017-08-30 18:47:38 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
-# code here!
+
+class School
+
+
+  attr_accessor :name, :roster, :student
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+   if @roster[grade]
+      @roster[grade]<< name
+    else
+      @roster[grade] = []
+      @roster[grade]<< name
+    end
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.map {|grade, name| [grade,name.sort] }.to_h
+  end
+
+end
